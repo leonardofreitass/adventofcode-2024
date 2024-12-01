451d6d2fcbcea9ae6dbae311bc369efcec6c6490
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2024-12-01 12:14:56 +0100

    feat: day 1 part 2

--- src/adventofcode_2024/exercises/day_1/part_2.clj
+++ src/adventofcode_2024/exercises/day_1/part_2.clj
@@ -1,5 +1,26 @@
-(ns adventofcode-2024.exercises.day-1.part-2)
+(ns adventofcode-2024.exercises.day-1.part-2
+  (:require [clojure.string :as str]))
+
+(defn count-occurrences [s slist]
+  (->> slist
+       flatten
+       (filter #{s})
+       count))
+
+(defn process-input
+  [inputs]
+  (reduce
+   (fn [[left right] line]
+     (let [[a b] (str/split line #"   ")]
+       [(conj left (Integer/parseInt a)) (conj right (Integer/parseInt b))]))
+   [[] []]
+   inputs))
 
 (defn run
   [inputs]
-  (nil))
+  (let [[left right] (process-input inputs)]
+    (reduce
+     (fn [acc a]
+       (+ acc (* a (count-occurrences a right))))
+     0
+     left)))
