08f89777a0edc09c3c2d537a7bd3d2a37c01a997
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2024-12-07 22:26:56 +0100

    feat: day 7 part 2

--- src/adventofcode_2024/exercises/day_7/part_2.clj
+++ src/adventofcode_2024/exercises/day_7/part_2.clj
@@ -1,4 +1,28 @@
 (ns adventofcode-2024.exercises.day-7.part-2)
 
+(defn parse-inputs
+  [inputs]
+  (map
+   #(map bigint (re-seq #"\d+" %))
+   inputs))
+
+(defn can-calculate
+  [target acc numbers]
+  (cond
+    (> acc target) false
+    (empty? numbers) (= acc target)
+    :else (or (can-calculate target (+ acc (first numbers)) (rest numbers))
+              (can-calculate target (* acc (first numbers)) (rest numbers))
+              (can-calculate target (bigint (str acc (first numbers))) (rest numbers)))))
+
 (defn run
-  [inputs])
+  [inputs]
+  (reduce
+   +
+   (bigint 0)
+   (map
+    first
+    (filter
+     (fn [[target acc & numbers]]
+       (can-calculate target acc numbers))
+     (parse-inputs inputs)))))
