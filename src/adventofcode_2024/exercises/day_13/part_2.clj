0f1093c5079a477b900449c93e839d1320218df0
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2024-12-13 18:24:34 +0100

    feat: day 13 part 2

--- src/adventofcode_2024/exercises/day_13/part_2.clj
+++ src/adventofcode_2024/exercises/day_13/part_2.clj
@@ -1,4 +1,38 @@
 (ns adventofcode-2024.exercises.day-13.part-2)
 
+(def offset 10000000000000)
+
+(defn get-numbers
+  [str]
+  (mapv #(Integer/parseInt %) (re-seq #"\d+" str)))
+
+(defn parse-block
+  [[a b prize]]
+  {:a (get-numbers a)
+   :b (get-numbers b)
+   :prize (mapv (partial + offset) (get-numbers prize))})
+
+(defn parse-inputs
+  [inputs]
+  (map
+   parse-block
+   (partition 3 4 inputs)))
+
+(defn solve-block
+  [{:keys [a b prize]}]
+  (let [[ax ay] a
+        [bx by] b
+        [px py] prize
+        an (abs (/ (- (* bx py) (* by px)) (- (* by ax) (* bx ay))))
+        bn (/ (- py (* ay an)) by)]
+    (+ (* 3 an) bn)))
+
 (defn run
-  [inputs])
+  [inputs]
+  (reduce
+   +
+   (filter
+    integer?
+    (map
+     solve-block
+     (parse-inputs inputs)))))
