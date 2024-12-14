9407cb42932e635cbcaef37c2e7e682e987a4cb7
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2024-12-14 11:48:03 +0100

    feat: day 14 part 2 (I am sorry, you want me to find a what?)

--- src/adventofcode_2024/exercises/day_14/part_2.clj
+++ src/adventofcode_2024/exercises/day_14/part_2.clj
@@ -1,4 +1,42 @@
-(ns adventofcode-2024.exercises.day-14.part-2)
+(ns adventofcode-2024.exercises.day-14.part-2 
+  (:require
+    [clojure.string :as str]))
+
+(def mod-arr [101 103])
+(def target "###############################")
+
+(def empty-grid
+  (vec (repeat (second mod-arr) (vec (repeat (first mod-arr) " ")))))
+
+(defn grid-with-robots
+  [robots]
+  (map
+   (partial str/join "")
+   (reduce
+    (fn [grid [[x y]]]
+      (assoc-in grid [y x] "#"))
+    empty-grid
+    robots)))
+
+(defn parse-inputs
+  [inputs]
+  (map (fn [input] (partition 2 (mapv #(Integer/parseInt %) (re-seq #"-?\d+" input)))) inputs))
+
+(defn pass-second
+  [robots]
+  (map
+   (fn [[initial speed]]
+     [(map
+       #(mod (+ %2 %3) %1)
+       mod-arr initial speed)
+      speed])
+   robots))
 
 (defn run
-  [inputs])
+  [inputs]
+  (loop [i 0
+         robots (parse-inputs inputs)]
+    (if (some #(str/includes? % target) (grid-with-robots robots))
+      i
+      (recur (inc i) (pass-second robots)))))
+ 
