189f82f234aaefb7ad834deccc74cfdce2113def
Leonardo Freitas dos Santos <leonardo.freitas.s@outlook.com>
2024-12-23 15:28:37 +0100

    feat: day 21 part 2

--- src/adventofcode_2024/exercises/day_23/part_2.clj
+++ src/adventofcode_2024/exercises/day_23/part_2.clj
@@ -1,4 +1,35 @@
-(ns adventofcode-2024.exercises.day-23.part-2)
+(ns adventofcode-2024.exercises.day-23.part-2 
+  (:require
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(defn plot-graph
+  [inputs]
+  (reduce
+   (fn [m [a b]]
+     (-> m
+         (update a (fnil conj #{}) b)
+         (update b (fnil conj #{}) a)))
+   {}
+   (map #(str/split % #"-") inputs)))
+
+(defn find-groups
+  [graph]
+  (reduce-kv
+   (fn [acc a s]
+     (assoc acc a (set (map #(conj (set/intersection s (graph %)) % a) s))))
+   {}
+   graph))
+
+(defn filter-connected-groups
+  [groups]
+  (filter
+   (fn [group]
+     (every? #(contains? (groups %) group) group))
+   (set (apply concat (vals groups)))))
 
 (defn run
-  [inputs])
+  [inputs]
+  (let [graph (plot-graph inputs)
+        groups (find-groups graph)]
+    (str/join "," (sort (last (sort-by count (filter-connected-groups groups)))))))
